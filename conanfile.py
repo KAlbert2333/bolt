cbd6cf3b77deb0ba3ed2b3376bb3d13f67fab44b
K·A <50450905+KAlbert2333@users.noreply.github.com>
2026-01-15 09:37:57 +0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -489,14 +489,28 @@ class BoltConan(ConanFile):
             tc.cache_variables["CMAKE_CXX_FLAGS"] = flags
             tc.cache_variables["CMAKE_C_FLAGS"] = flags
 
+        #Check SVE
+        import subprocess
+        sve_supported = False
+        result = subprocess.run(['lscpu'], capture_output=True, text=True, timeout=3)
+        if result.returncode == 0 and 'sve' in result.stdout.lower():
+            sve_supported = True
+            
         if str(self.settings.arch) in ["armv8", "arm"]:
-            # Support CRC & NEON & SVE on ARMv8
-            flags = f"{self.BOLT_GLOABL_FLAGS} -march=armv8.3-a+sve -msve-vector-bits=256 -DSVE_BITS=256"
+            if sve_supported:
+                # Support CRC & NEON & SVE on ARMv8
+                flags = f"{self.BOLT_GLOABL_FLAGS} -march=armv8.3-a+sve -msve-vector-bits=256 -DSVE_BITS=256"
+            else:
+                # Support CRC & NEON on ARMv8
+                flags = f"{self.BOLT_GLOABL_FLAGS} -march=armv8.3-a"
             tc.cache_variables["CMAKE_CXX_FLAGS"] = flags
             tc.cache_variables["CMAKE_C_FLAGS"] = flags
         elif str(self.settings.arch) in ["armv9"] and not is_msvc(self):
             # gcc 12+ https://www.phoronix.com/news/GCC-12-ARMv9-march-armv9-a
-            flags = f"{self.BOLT_GLOABL_FLAGS} -march=armv9-a+sve -msve-vector-bits=256 -DSVE_BITS=256"
+            if sve_supported:
+                flags = f"{self.BOLT_GLOABL_FLAGS} -march=armv9-a+sve -msve-vector-bits=256 -DSVE_BITS=256"
+            else:
+                flags = f"{self.BOLT_GLOABL_FLAGS} -march=armv9-a"
             tc.variables["CMAKE_C_FLAGS"] = flags
             tc.variables["CMAKE_CXX_FLAGS"] = flags
 
